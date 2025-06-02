86798a1213329b425ac9438fa8195c36d5a5cf39
MAYSSAE AZIRAR <m.azirar@esisa.ac.ma>
2025-06-03 00:22:15 +0100

    add sec1

---
+++ index.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("toggle");
+const navbar1 = document.getElementById("navbar1");
+
+toggle.addEventListener("click", () => {
+    navbar1.classList.toggle("show");
+});
